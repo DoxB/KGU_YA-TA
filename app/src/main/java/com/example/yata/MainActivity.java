7e6b2e4973dea45beb1cc0c24f46e9ef95136310
na0th <105775683+na0th@users.noreply.github.com>
2023-01-30 21:28:05 +0900

    slide

--- app/src/main/java/com/example/yata/MainActivity.java
+++ app/src/main/java/com/example/yata/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.yata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -17,14 +22,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//      DrawerLayout(슬라이드 메뉴바) 객체 생성
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer);
+//      이미지 뷰 객체 생성해서 id로 이벤트 처리
+        ImageView imageView = (ImageView)findViewById(R.id.iv_menu);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!drawer.isDrawerOpen(Gravity.LEFT)){
+                    drawer.openDrawer(Gravity.LEFT);
+                }
+                if(drawer.isDrawerOpen(Gravity.LEFT)){
+                    drawer.closeDrawers();
+                }
+            }
+        });
+
+
         busFragment = new BusFragment();
         subwayFragment = new SubwayFragment();
         navFragment = new NavFragment();
 
         getSupportFragmentManager().beginTransaction().add(R.id.mainFrame, busFragment).commit();
-
         TabLayout tabs = (TabLayout) findViewById(R.id.tabLayout);
-
         tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
