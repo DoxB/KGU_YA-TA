87f8186a791df35819cce3bd320375068d8e50c8
DoxB <jg.doxb@gmail.com>
2023-02-07 20:15:58 +0900

    won

--- README.md
+++
@@ -1,5 +0,0 @@
-# KGU_YA-TA
-
-
-ICON 쓴 거 링크를 달아야 함.
-![img.png](img.png) <- 참고하기
--- app/src/main/java/com/example/yata/MainActivity.java
+++ app/src/main/java/com/example/yata/MainActivity.java
@@ -1,85 +1,216 @@
 package com.example.yata;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.android.material.tabs.TabLayout;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit;
+    TextView text;
+    XmlPullParser xpp;
 
-    Fragment busFragment, subwayFragment, navFragment;
-
+    String key="자신의 서비스키";
+    String data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//      DrawerLayout(슬라이드 메뉴바) 객체 생성
-        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer);
-//      이미지 뷰 객체 생성해서 id로 이벤트 처리
-        ImageView imageView = (ImageView)findViewById(R.id.iv_menu);
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//클릭시 Gravity.left는 android:layout_gravity="left"인 View나 Layout을 감지하여 옆으로 연다
-                if(!drawer.isDrawerOpen(Gravity.LEFT)){
-                    drawer.openDrawer(Gravity.LEFT);
-                }
-                if(drawer.isDrawerOpen(Gravity.LEFT)){
-                    drawer.closeDrawers();
-                }
-            }
-        });
-
-
-        busFragment = new BusFragment();
-        subwayFragment = new SubwayFragment();
-        navFragment = new NavFragment();
-
-        getSupportFragmentManager().beginTransaction().add(R.id.mainFrame, busFragment).commit();
-        TabLayout tabs = (TabLayout) findViewById(R.id.tabLayout);
-        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-
-                int position = tab.getPosition();
-
-                Fragment selected = null;
-                if(position == 0){
-
-                    selected = busFragment;
-
-                }else if (position == 1){
-
-                    selected = subwayFragment;
-
-                }else if (position == 2){
-
-                    selected = navFragment;
-
-                }
-                getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, selected).commit();
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+        edit= (EditText)findViewById(R.id.edit);
+        text= (TextView)findViewById(R.id.result);
 
     }
+    public void mOnClick(View v){
+        switch (v.getId()){
+            case R.id.button:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        data=getXmlData();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                text.setText(data);
+                            }
+                        });
+                    }
+                }).start();
+                break;
+        }
+    }
+    String getXmlData(){
+        StringBuffer buffer=new StringBuffer();
+        String str= edit.getText().toString();//EditText에 작성된 Text얻어오기
+        String location = URLEncoder.encode(str);
+        String apiKey ="AfP3v76bOSIU39GkxRfTcr8TdXXjuNzLYgWfig88/gY";
+        String queryUrl = "https://api.odsay.com/v1/api/searchBusLane?apiKey="+apiKey+"&busNo="+location+"&output=xml";
+
+        try{
+            URL url = new URL(queryUrl);
+            InputStream is= url.openStream(); //url위치로 입력스트림 연결
+
+            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();//xml파싱을 위한
+            XmlPullParser xpp= factory.newPullParser();
+            xpp.setInput( new InputStreamReader(is, "UTF-8") ); //inputstream 으로부터 xml 입력받기
+
+            String tag;
+
+            xpp.next();
+            int eventType= xpp.getEventType();
+            while( eventType != XmlPullParser.END_DOCUMENT ){
+                switch( eventType ){
+                    case XmlPullParser.START_DOCUMENT:
+                        buffer.append("파싱 시작...\n\n");
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        tag= xpp.getName();//테그 이름 얻어오기
+
+                        if(tag.equals("item")) ;// 첫번째 검색결과
+                        else if(tag.equals("cityName")){
+                            buffer.append("도시명 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//title 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("CID")){
+                            buffer.append("도시코드 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//category 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n");//줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busID")){
+                            buffer.append("버스노선코드 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//description 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n");//줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busStartPoint")){
+                            buffer.append("버스노선 기점 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//telephone 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n");//줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("bus_Interval_Week")){
+                            buffer.append("평일 배차간격 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//address 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n");//줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busCityCode")){
+                            buffer.append("운수회사 승인 도시코드 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapx 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("  ,  "); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("type")){
+                            buffer.append("버스노선 종류 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busEndPoint")){
+                            buffer.append("버스노선 종점 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("bus_Interval_Sun")){
+                            buffer.append("일요일(공휴일) 배차간격 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("localBusID")){
+                            buffer.append("각 지역버스노선 ID :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busCityName")){
+                            buffer.append("운수회사 승인 도시이름 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busInterval")){
+                            buffer.append("운행횟수 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busCompanyNameKor")){
+                            buffer.append("운수회사명 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("bus_Interval_Sat")){
+                            buffer.append("토요일 배차간격 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busFirstTime")){
+                            buffer.append("첫차시간 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busCompanyID")){
+                            buffer.append("운수회사ID :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busNo")){
+                            buffer.append("버스번호 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("busLastTime")){
+                            buffer.append("막차시간 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        tag= xpp.getName(); //테그 이름 얻어오기
+
+                        if(tag.equals("item")) buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
+                        break;
+                }
+
+                eventType= xpp.next();
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        buffer.append("파싱 끝\n");
+        return buffer.toString();//StringBuffer 문자열 객체 반환
+
+    }//getXmlData method....
+
 }
